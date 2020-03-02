677d41e31325c6552bceaafc551757c399760e11
One0fAKind <34012881+One0fAKind@users.noreply.github.com>
2020-03-02 14:39:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello World");
+import unittest
+
+import function
+
+class TestMyFunction(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def testadder(self):
+        self.assertEqual( function.adder(1), 2)
+      
+if __name__ == '__main__':
+  unittest.main()
